85b3385876935c105b857b46777f391c8b5bc87d
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-20 21:25:11 +0200

    pi-autosave: write call_53v6ogap 2026-04-20T19:25:10.905Z

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' }
+    ],
+    formats: ['image/avif', 'image/webp']
+  }
+};
+
+module.exports = nextConfig;
