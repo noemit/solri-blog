df3301d9ba9ef84ebe1793992c96379325047a46
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-22 16:48:08 +0200

    pi-autosave: bash call_eu5a7n6f 2026-04-22T14:48:08.088Z

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = {};
+module.exports = {
+  experimental: {
+    // Try disabling worker usage
+    workerThreads: false,
+    optimizePackageImports: false,
+  },
+  compiler: {
+    // Use SWC instead of Babel for faster builds
+    target: 'es2020',
+  },
+}
