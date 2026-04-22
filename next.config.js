5f62c9eaa398711ba32bbf63ef9d5f8ca2df74aa
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-22 14:36:05 +0200

    pi-autosave: bash call_wt0tqlti 2026-04-22T12:36:05.744Z

--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // Disable webpack to avoid Turbopack conflict on Windows
-  experimental: {
-    // Try to avoid worker issues
-    webpack: (config) => config
-  }
-};
+module.exports = {};
