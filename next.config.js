f55d155bc6f3225312bfce6f419421530caaf854
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-22 13:10:12 +0200

    pi-autosave: edit call_l64pgw0v 2026-04-22T11:10:12.243Z

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  typescript: {
-    // Fixes the jest-worker error
-    diagnosticOption: "preserve" 
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  },
   webpack: (config, { isServer }) => {
     config.cache = false;
-    config.resolve = config.resolve || {};
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      // Handle worker issues
-      'node:buffer': require.resolve('buffer'),
-      'node:util': require.resolve('util'), 
-      'node:stream': require.resolve('stream'),
-      'node:crypto': require.resolve('crypto'),
-    };
     return config;
   },
 };
