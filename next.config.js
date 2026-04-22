fca46fa8bdb6dbe774ff32b7766d1d5b5caa996e
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-22 13:00:31 +0200

    pi-autosave: edit call_vpafqs02 2026-04-22T11:00:31.178Z

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   webpack: (config, { isServer }) => {
+    // Fix jest-worker error on Windows
     config.cache = false;
+    if (!isServer && config.externals) {
+      config.externals = config.externals.filter(
+        (e) => !e.includes('jest-util')
+      );
+    }
     return config;
   },
 };
