2118ddb0922293af66206876f8228943053fe1b9
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-24 00:21:48 +0200

    pi-autosave: edit call_52b3bs36 2026-04-23T22:21:48.677Z

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    workerThreads: false,
-    optimizeFonts: false, // Disable built-in NPM font optimization
-  },
-  // Disable Next.js font optimization - we'll use Google Fonts directly
   trailingSlash: true,
   images: {
     remotePatterns: [
