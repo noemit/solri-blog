b25eac4039061bc122c0daf9459499ae773fca76
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-23 21:15:01 +0200

    pi-autosave: write call_f0lxuqxq 2026-04-23T19:15:00.765Z

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// Use absolute imports to access eslint-config-next modules from node_modules
+const eslintNextCoreWebVitals = require.resolve("eslint-config-next/core-web-vitals");
+const eslintNextTypescript = require.resolve("eslint-config-next/typescript");
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    plugins: {
+      "next/core-web-vitals": eslintNextCoreWebVitals,
+      "next/typescript": eslintNextTypescript,
+    },
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "node_modules/**",
+    ],
+  },
 ]);
 
 export default eslintConfig;
