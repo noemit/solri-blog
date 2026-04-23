a88486b68d859cee5c506d4c9cd1ba503e4af8c2
sol-ri <noemi.titarenco+solri@gmail.com>
2026-04-23 21:34:48 +0200

    pi-autosave: write call_0lbw59ak 2026-04-23T19:34:47.764Z

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.page.{js,ts,jsx,tsx}',
+    './app/**/**/*.page.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
